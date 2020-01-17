8606a8ed874f6e7bb636cfc52270f4aaadb38245
Vinicius Farias Santos <spviniciusfariassantos@gmail.com>
2020-01-17 10:36:38 -0300

    Projeto Finalizado

---
+++ Veiculo_2/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veiculo_2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+
+
+            Program carroteste;
+            carroteste = new Program();
+			carroteste.ligar();
+			carroteste.acelerar(0);
+
+			carroteste.acelerar(20);
+			carroteste.acelerar(20);
+			carroteste.acelerar(20);
+			carroteste.acelerar(20);
+			carroteste.frear(20);
+			carroteste.desligado();
+			carroteste.abastecer(10);
+
+			carroteste.frear(20);
+			carroteste.frear(20);
+			carroteste.frear(20);
+			carroteste.frear(20);
+
+
+
+
+
+
+			carroteste.desligado();
+			carroteste.acelerar(1);
+			carroteste.acelerar(1);
+			carroteste.abastecer(1);
+			carroteste.abastecer(2);
+
+			//carro.frear(20);
+			//carro.frear(20);
+			//carro.frear(20);
+
+
+
+
+
+		}
+	}
+}
---
+++ Veiculo_2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Veiculo_2
+{
+    class Program
+    {
+
+        public string cor = "Azul";
+        public string Marca;
+        public string Modelo;
+        public string placa;
+
+        public double km;
+        public bool isLigado;
+        public int combustiveis = 100;
+        public int velocidade = 20;
+        public double preco;
+
+
+
+
+        public void ligar()
+        {
+            isLigado = true;
+            Console.WriteLine("O Carro esta ligado e andando a  " +  velocidade +" km/h");
+
+        }
+
+
+
+
+
+        public void acelerar(int quantidade)
+        {
+            if (isLigado)
+            {
+                velocidade += quantidade;
+                combustiveis--;
+                Console.WriteLine("Você acelerou, seu veiculos está  a   " + velocidade + " km/h ");
+            }
+            else
+            {
+                Console.WriteLine("O Carro esta desligado");
+            }
+        }
+        
+        
+        
+        
+        
+        
+        public void frear(int quantidade)
+        {
+            if ((isLigado == true)&&(velocidade != 0))
+            {
+                velocidade -= quantidade;
+
+                Console.WriteLine("Você freou, seu veiculo está a  " + velocidade + " km/h ");
+            }
+            else
+            {
+
+                Console.WriteLine("O Carro esta desligado");
+            }
+        }
+
+        public void abastecer(int qtdLitros)
+        {
+            
+            if (combustiveis < 100)
+            {
+                if (isLigado == false)
+                {
+
+                    combustiveis = combustiveis + qtdLitros;
+
+                    if (combustiveis > 100)
+                    {
+                        int x = combustiveis - 100;
+                        combustiveis = combustiveis - x;
+
+                        Console.WriteLine("seu limite de abastecer excedeu, limitamos  até " + combustiveis + "  litros  para evitar acidente.");
+
+
+                    }
+
+                    Console.WriteLine("seu tanque está  " + combustiveis + "  litros");
+
+                }
+                else
+                {
+
+
+                    Console.WriteLine("Seu carro esta em movimento, você não pode abastecer");
+
+
+                }
+                
+            }
+        }
+        
+        
+        
+        
+        
+        public void desligado() {
+
+           
+
+            if (velocidade == 0) {
+
+                isLigado = false;
+
+                Console.WriteLine("Seu carro está desligado");
+
+            }
+            else {
+                Console.WriteLine("Você precisa Frear o carro, para poder desligar  ");
+            }
+        
+        }
+
+
+
+        void pintar(String novCor)
+        {
+            Console.WriteLine("Sua cor atual é  " + cor);
+            Console.WriteLine("Escolhe uma cor ");
+            cor = novCor;
+            Console.WriteLine("Sua nova cor é " + cor);
+        }
+
+
+
+    }
+
+}
+
